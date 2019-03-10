6e50b5bedc921cfc568deb5f48473528d26a238e
Rob Middleton <rob@middlerob.com>
2019-03-10 16:46:39 -0700

    Fix for fastmail updates, keep polling even if there's an error

--- fastmail-favicon-unread-count.user.js
+++ fastmail-favicon-unread-count.user.js
@@ -4,24 +4,19 @@
 // @license     MIT
 // @description Adds a dynamic favicon to fastmail.com showing unread emails.
 // @namespace   rob@middlerob.com
-// @version     1.2.0
 // @grant       none
+// @version     2.1.0
 // @include     https://www.fastmail.com/mail/*
 // ==/UserScript==
 
 ;(function() {
-  // Edit here to change excluded folders
-  // To exclude sub folders just separate with a > like: "My Folder > Sub Folder"
-  var excludeFolders = ['Spam', 'Trash', 'Promotions', 'Events']
-
   var totalUnread = '?'
   var pageFocused = false
   var focusedSinceNew = true
   var animateFlashesRemaining = 0
   var animateFlashOn
 
-  var listeners = []
-  var favicon = document.querySelector("link[sizes='32x32']")
+  var favicon = document.querySelector("link[sizes='64x64']")
 
   var defaultImage = new Image()
   defaultImage.onload = draw
@@ -30,7 +25,7 @@
   var canvas = document.createElement('canvas')
   var ctx = canvas.getContext('2d')
 
-  var faviconWidth = 16
+  var faviconWidth = 64
   canvas.width = faviconWidth
   canvas.height = faviconWidth
 
@@ -47,24 +42,24 @@
     }
 
     if (highlight) {
-      ctx.drawImage(defaultImage, 0, 0, 16, 16)
+      ctx.drawImage(defaultImage, 0, 0, faviconWidth, faviconWidth)
     } else {
       ctx.fillStyle = '#ff2323'
       ctx.fillRect(0, 0, faviconWidth, faviconWidth)
     }
 
-    ctx.font = 'bold 14px Arial'
+    ctx.font = 'bold ' + faviconWidth * 0.8 + 'px Arial'
     ctx.fillStyle = 'white'
     ctx.strokeStyle = 'black'
 
     var textWidth = ctx.measureText(text).width
     var centered = faviconWidth / 2 - textWidth / 2
     if (textWidth <= faviconWidth) {
-      ctx.strokeText(text, centered, 13)
-      ctx.fillText(text, centered, 13)
+      ctx.strokeText(text, centered, faviconWidth * 0.8)
+      ctx.fillText(text, centered, faviconWidth * 0.8)
     } else {
-      ctx.strokeText(text, 0, 13, faviconWidth)
-      ctx.fillText(text, 0, 13, faviconWidth)
+      ctx.strokeText(text, 0, faviconWidth * 0.8, faviconWidth)
+      ctx.fillText(text, 0, faviconWidth * 0.8, faviconWidth)
     }
 
     favicon.href = canvas.toDataURL('image/png')
@@ -88,20 +83,10 @@
     pageFocused = false
   })
 
-  function nameForBox(box) {
-    var parent = box.get('parent')
-    var name = box.get('name')
-    return parent ? parent.get('name') + ' > ' + name : name
-  }
-
   function updateTotal() {
-    var unread = window.FastMail.allMailboxes
-      .filter(function(box) {
-        return excludeFolders.indexOf(nameForBox(box)) === -1
-      })
-      .reduce(function(total, current) {
-        return total + current.get('unread')
-      }, 0)
+    var el = document.querySelector('.v-MailboxSource--inbox .v-MailboxSource-badge')
+    if (!el) return
+    var unread = parseInt(el.innerHTML, 10)
 
     if (unread > parseInt(totalUnread, 10)) {
       // new unread message
@@ -118,43 +103,10 @@
     draw()
   }
 
-  function reBind() {
-    var listener
-    while ((listener = listeners.pop())) {
-      listener[0].removeObserverForKey(listener[1], listener[2], listener[3])
-    }
-
-    window.FastMail.allMailboxes.forEach(function(box) {
-      var handler = {
-        fn: function() {
-          updateTotal()
-        },
-      }
-      box.addObserverForKey('unread', handler, 'fn')
-      listeners.push([box, 'unread', handler, 'fn'])
-    })
+  function pollForChanges() {
+    setTimeout(pollForChanges, 1000)
+    updateTotal()
   }
 
-  function tryInit() {
-    // wait for FastMail to load by polling
-    if (window.FastMail.allMailboxes) {
-      window.FastMail.allMailboxes.addObserverForRange(
-        {},
-        {
-          fn: function() {
-            reBind()
-            updateTotal()
-          },
-        },
-        'fn'
-      )
-      reBind()
-      updateTotal()
-    } else {
-      // try again in 1s
-      setTimeout(tryInit, 1000)
-    }
-  }
-
-  tryInit()
+  pollForChanges()
 })()
